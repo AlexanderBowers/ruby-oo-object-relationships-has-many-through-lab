018397920343ccd8ec4845ab7d90cfe3944c806c
bowers <7319abowers@gmail.com>
2020-11-23 11:09:18 -0800

    finished lab

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,29 @@
+class Artist
+    @@all = []
 
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def new_song(song, genre)
+        song = Song.new(song, self, genre)
+    end
+    
+    def songs
+        Song.all.select {|songs| songs.artist == self }
+    end
+
+    def genres
+        genre = []
+        self.songs.select{|song| genre << song.genre}
+        genre.uniq
+    end
+
+end
